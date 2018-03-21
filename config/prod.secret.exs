460571c75415c12e19ce6ca59eabbd9129e2ed74
glyn <glynnnnnnnnn@gmail.com>
2018-03-20 23:24:41 -0400

    add key to repo

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :candy_crush_mega, CandyCrushMegaWeb.Endpoint,
+  secret_key_base: "OKZzyfiHXl0ZJ1UMeqhyxXyFYXFT1Ve8uDT6DbR50SnwMp+kNULuRhTrl8sd2E2B"
